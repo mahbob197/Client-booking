59dee2dc2f88fc6249f618c6253a2308bb8e1549
mahbob197 <108810998+mahbob197@users.noreply.github.com>
2023-05-16 16:59:19 +0000

    Add files via upload

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import "./navbar.css";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+
+
+const Navbar = () => {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">Abrajeimmo</span>
+        </Link>
+        {user ? user.username : (
+         <div className="navItems">
+
+         <Link to="/login" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="login">Login</span>
+        </Link>
+
+        
+     
+
+          </div>
+    
+        )}
+
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
